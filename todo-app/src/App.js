feae3954ca62501989e05bcc02d64bee5942599c
Prathima Maheshwaram <prathimamaheshwaram@Prathimas-MacBook-Pro.local>
2022-08-04 16:23:59 +0530

    react-project-setup boilerplate code

---
+++ todo-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-green-500 bg-red-50">lets get started!</div>
+    </div>
+  );
+}
+
+export default App;
